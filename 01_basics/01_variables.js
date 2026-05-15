9b5adfddd31172c1a6395a7f2f0559520e6baee5
PriPaan <pandey02.priyanshu@gmail.com>
2026-05-15 07:43:56 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14453
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2
+accountEmail = "hc@hc.com"
+accountPassword = "211122"
+accountCity = "Beangaluru"
+
+console.groupCollapsed(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// alert(3+3)
+
+let name = "Priyanshu"
+let age = 18
+let isLoggedIn = false
+

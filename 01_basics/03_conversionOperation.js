29cfb7ea31a0f05f3667896d64a671b5d094fd2a
PriPaan <pandey02.priyanshu@gmail.com>
2026-05-15 07:58:14 +0000

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+let score = undefined
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn = Boolean(0)
+let boolenLoggedIn = Boolean(isLoggedIn)
+console.log(isLoggedIn)

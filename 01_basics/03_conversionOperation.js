7f1a33b9e40a9af48ebc1bb972802ef8bd408e32
PriPaan <pandey02.priyanshu@gmail.com>
2026-05-15 09:21:24 +0000

    data Type Summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -9,3 +9,40 @@ console.log(valueInNumber)
 let isLoggedIn = Boolean(0)
 let boolenLoggedIn = Boolean(isLoggedIn)
 console.log(isLoggedIn)
+
+//***************Operations ***********************
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(2+2)
+
+let str1 = "hello"
+let str2 = "Priyanshu"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log(+1)
+
+console.log("2"==2)
+console.log("2"===2)
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+const bigNumber = 1233838838393993n
+
+const heros = ["shaktiman","naagraj","doga"]
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world")
+}
+
+console.log(typeof myFunction)
